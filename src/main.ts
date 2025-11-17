6d2178aec1efe09074d990f16d1af9bb20022c62
Jose Carlos Efrain Cortez Carrera <jose.cortez@elektra.com.gt>
2025-11-16 23:43:31 -0600

    change port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3005);
+  await app.listen(8080);
   console.log("🚀 Backend API corriendo en http://localhost:3005");
 }
 bootstrap();
